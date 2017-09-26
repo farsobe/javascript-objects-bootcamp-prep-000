bd2ba4f59464159fb1295d63dcc2e63a93a0a154
Farrell Sobejana <fsobejana+flatiron@gmail.com>
2017-09-26 18:05:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -14,24 +14,9 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle;
   return playlist;
 }
-/*
-function removeFromPlaylist(playlist, artistName){
-  var playlist {};
-  delete playlist.artistName;
-  return playlist;
-}
-*/
-/*
-var playlist = {artistName: "songTitle"};
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = songTitle;
-  return playlist;
-}
 
 function removeFromPlaylist(playlist, artistName){
   var playlist = {};
   delete playlist.artistName;
   return playlist;
 }
-*/
